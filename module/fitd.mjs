8420585cd429e35487df8252305d7ba7e60d9b97
Johnnie Pittman <jpittman@group6.net>
2025-02-21 00:53:18 -0500

    Make use of the data models we've started to define.

--- module/fitd.mjs
+++ module/fitd.mjs
@@ -7,6 +7,8 @@ import { FitdItemSheet } from './sheets/item-sheet.mjs';
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from './helpers/templates.mjs';
 import { FITD } from './helpers/config.mjs';
+// Import DataModel classes
+import * as models from './data/_module.mjs';
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -33,9 +35,22 @@ Hooks.once('init', function () {
     decimals: 2,
   };
 
-  // Define custom Document classes
+  // Define custom Document and DataModel classes
   CONFIG.Actor.documentClass = FitdActor;
+
+  // Note that you don't need to declare a DataModel
+  // for the base actor/item classes - they are included
+  // with the Character/NPC as part of super.defineSchema()
+  CONFIG.Actor.dataModels = {
+    character: models.FitdCharacter,
+    npc: models.FitdNPC
+  }
   CONFIG.Item.documentClass = FitdItem;
+  CONFIG.Item.dataModels = {
+    item: models.FitdItem,
+    feature: models.FitdFeature,
+    spell: models.FitdSpell
+  }
 
   // Active Effects are never copied to the Actor,
   // but will still apply to the Actor from within the Item
