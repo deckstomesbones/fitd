0131b685ac788ca650279bb4953c19cd4eeab7b9
Johnnie Pittman <jpittman@group6.net>
2025-02-21 00:55:53 -0500

    Comment out the old code for generating abilities (think dnd attributes).
    Duplicate the generating schema to use for actions.
    
    Also do a similar thing with derived data for now.

--- module/data/character.mjs
+++ module/data/character.mjs
@@ -13,11 +13,20 @@ export default class FitdCharacter extends FitdActorBase {
       }),
     });
 
+    // // Iterate over ability names and create a new SchemaField for each.
+    // schema.abilities = new fields.SchemaField(Object.keys(CONFIG.FITD.abilities).reduce((obj, ability) => {
+    //   obj[ability] = new fields.SchemaField({
+    //     value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
+    //     mod: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+    //     label: new fields.StringField({ required: true, blank: true })
+    //   });
+    //   return obj;
+    // }, {}));    
+    
     // Iterate over ability names and create a new SchemaField for each.
-    schema.abilities = new fields.SchemaField(Object.keys(CONFIG.FITD.abilities).reduce((obj, ability) => {
-      obj[ability] = new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
-        mod: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
+    schema.actions = new fields.SchemaField(Object.keys(CONFIG.FITD.actions).reduce((obj, action) => {
+      obj[action] = new fields.SchemaField({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 }),
         label: new fields.StringField({ required: true, blank: true })
       });
       return obj;
@@ -28,11 +37,14 @@ export default class FitdCharacter extends FitdActorBase {
 
   prepareDerivedData() {
     // Loop through ability scores, and add their modifiers to our sheet output.
-    for (const key in this.abilities) {
-      // Calculate the modifier using d20 rules.
-      this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
-      // Handle ability label localization.
-      this.abilities[key].label = game.i18n.localize(CONFIG.FITD.abilities[key]) ?? key;
+    // for (const key in this.abilities) {
+    //   // Calculate the modifier using d20 rules.
+    //   this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
+    //   // Handle ability label localization.
+    //   this.abilities[key].label = game.i18n.localize(CONFIG.FITD.abilities[key]) ?? key;
+    // }
+    for (const key in this.actions) {
+      this.actions[key].label = game.i18n.localize(CONFIG.FITD.actions[key]) ?? key;
     }
   }
 
