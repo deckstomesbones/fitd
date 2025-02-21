71ae7457d07d4588eab90b3399cba6fa3cd0602a
Johnnie Pittman <jpittman@group6.net>
2025-02-21 00:58:25 -0500

    Update the english language file to include actions and attributes.
    add actions and attributes to the helper config file.

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -21,3 +21,24 @@ FITD.abilityAbbreviations = {
   wis: 'FITD.Ability.Wis.abbr',
   cha: 'FITD.Ability.Cha.abbr',
 };
+
+FITD.attributes = {
+  insight: 'FITD.Attribute.Insight.name',
+  prowess: 'FITD.Attribute.Prowess.name',
+  resolve: 'FITD.Attribute.Resolve.name'
+};
+
+FITD.actions = {
+  hunt: 'FITD.Attribute.Insight.Action.Hunt',
+  study: 'FITD.Attribute.Insight.Action.Study',
+  survey: 'FITD.Attribute.Insight.Action.Survey',
+  tinker: 'FITD.Attribute.Insight.Action.Tinker',
+  finesse: 'FITD.Attribute.Prowess.Action.Finesse',
+  prowl: 'FITD.Attribute.Prowess.Action.Prowl',
+  skirmish: 'FITD.Attribute.Prowess.Action.Skirmish',
+  wreck: 'FITD.Attribute.Prowess.Action.Wreck',
+  attune: 'FITD.Attribute.Resolve.Action.Attune',
+  command: 'FITD.Attribute.Resolve.Action.Command',
+  consort: 'FITD.Attribute.Resolve.Action.Consort',
+  sway: 'FITD.Attribute.Resolve.Action.Sway'
+};
